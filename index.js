3a9212c16ac39e0da2d7d71e7b5ad319c6e23303
AnkitAKS02 <ankitkumarswain2003@gmail.com>
2026-06-07 16:27:34 +0530

    noting new just try one more time

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.post("/api/data/moredata/:id", (req, res) => {
     res.json({ message: "Data received successfully!", receivedData: data });
 });
 
+app.put("/api/data/update/:id", (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    console.log("Received data:", data);
+    console.log("Received ID:", id);
+    res.json({ message: "Data updated successfully!", receivedData: data });
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
